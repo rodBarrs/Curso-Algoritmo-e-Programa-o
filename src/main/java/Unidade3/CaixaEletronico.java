a58cec677197dd7da6a9056c6ce832c92b1930c4
Rodrigo Barros <rodrigosbarros51@gmail.com>
2022-09-19 22:18:10 -0300

    Aula 55

---
+++ src/main/java/Unidade3/CaixaEletronico.java
@@ -0,0 +1,34 @@
+package Unidade3;
+
+import javax.swing.*;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        int saque;
+        double notas100, notas50, notas10;
+        int resto;
+
+        saque = Integer.parseInt(JOptionPane.showInputDialog("Favor informar o valor do saque: "));
+
+        if (saque%10 == 0){
+            notas100 = Math.floor(saque/100);
+            resto = saque % 100;
+            notas50 = Math.floor(resto/50);
+            resto = resto % 50;
+            notas10 = Math.floor(resto/10);
+
+            if (notas100 > 0){
+                System.out.println("O número de notas de cem= "+ notas100);
+            }
+            if (notas50 > 0){
+                System.out.println("O número de notas de cinquenta= "+ notas50);
+            }
+            if (notas10 > 0){
+                System.out.println("O número de notas de dez= "+ notas10);
+            }
+        }else {
+            System.out.println("Valor informado do saque está incorreto!");
+        }
+
+    }
+}
