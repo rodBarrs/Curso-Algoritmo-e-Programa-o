f003da68d681a24bf74f980088e2573918834804
Rodrigo Barros <rodrigosbarros51@gmail.com>
2022-09-19 21:33:27 -0300

    Aula 52

--- src/main/java/Unidade3/AvaliacaoNota.java
+++ src/main/java/Unidade3/AvaliacaoNota.java
@@ -1,2 +1,26 @@
-package Unidade3;public class AvaliacaoNota {
+package Unidade3;
+
+import java.util.Scanner;
+
+public class AvaliacaoNota {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o valor da nota: ");
+        var nota = teclado.nextInt();
+
+        if ((nota >=0) & (nota <= 100)){
+            if (nota >= 90){
+                System.out.println("Conceito excelente ");
+            } else if (nota >= 70){
+                System.out.println("Conceito Bom ");
+            } else if (nota > 50){
+                System.out.println("Conceito Regular ");
+            } else {
+                System.out.println("Conceito Insuficiente ");
+            }
+        }else{
+            System.out.println("Nota inválida ");
+            AvaliacaoNota.main(null);
+        }
+    }
 }
--- src/main/java/Unidade3/IdadeNadador.java
+++ src/main/java/Unidade3/IdadeNadador.java
@@ -1,2 +1,31 @@
-package Unidade3;public class IdadeNadador {
+package Unidade3;
+
+import java.util.Scanner;
+
+public class IdadeNadador {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite a idade: ");
+        var idade = teclado.nextInt();
+
+        if ((idade >=0) & (idade <= 100)){
+            if (idade <= 5){
+                System.out.println("Infantil A");
+            } else if (idade <= 8){
+                System.out.println("Infantil B");
+            } else if (idade <= 11){
+                System.out.println("Infantil C");
+            } else if (idade <= 13){
+                System.out.println("Juvenil A");
+            } else if (idade <= 17){
+                System.out.println("Juvenil B");
+            } else {
+                System.out.println("Adulto");
+            }
+        }else{
+            System.out.println("Idade inválida ");
+            AvaliacaoNota.main(null);
+        }
+
+    }
 }
