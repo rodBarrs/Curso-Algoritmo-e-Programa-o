ce9f23a3e963b02447864b81848d416de6641ad3
rodBarrs <rodrigosbarros51@gmail.com>
2022-09-23 13:49:38 -0300

    Aula 75

---
+++ src/main/java/Unidade4/CalculaMediav2.java
@@ -0,0 +1,32 @@
+package Unidade4;
+
+import java.util.Scanner;
+
+public class CalculaMediav2 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Favor Informar o Valor: ");
+        var valor = teclado.nextInt();
+
+        var somaValor = 0;
+        var cont = 0;
+
+       do{
+           if ( valor != -1){
+               somaValor += valor;
+               cont++;
+               System.out.println("Favor Informar o Valor: ");
+               valor = teclado.nextInt();
+           }
+
+
+
+        }while(valor != -1);
+       if (cont != 0){
+           System.out.println("Média: "+ somaValor/cont);
+       }
+
+
+    }
+}
---
+++ src/main/java/Unidade4/NumerosPares.java
@@ -0,0 +1,7 @@
+package Unidade4;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        
+    }
+}
